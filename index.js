73cb38d7416e57cc7cade5ca6aef94c195f83853
Igor Molina <igoramil452@gmai.com>
2024-02-19 19:33:38 -0300

    Dando continuidade: casos mais complicados

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let addresses = ["Miritiba 339", "Babaçu 500", "Cambuí 804B"];
+let addresses = [
+  "Miritiba 339",
+  "Babaçu 500",
+  "Cambuí 804B",
+  "Rio Branco 23",
+  "Quirino dos Santos 23 b",
+];
 
 let address = {
   street: "",
@@ -6,7 +12,7 @@ let address = {
 };
 
 for (let i = 0; i < addresses.length; i++) {
-  let splitAddress = addresses[i].split(/\s/);
+  let splitAddress = addresses[i].split(/\s(?=\d)/);
 
   address.number = splitAddress.pop();
   address.street = splitAddress.join(" ");
