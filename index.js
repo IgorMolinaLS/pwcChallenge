0fcc5bb881d447583a6a9db362c00f35952003fe
Igor Molina <igoramil452@gmai.com>
2024-02-20 19:48:56 -0300

    Refatoração do código

--- index.js
+++ index.js
@@ -17,43 +17,43 @@ let address = {
 
 //Helper Functions:
 //Cria um array secundário com 'Word' e 1 dependendo do dado do endereço recebido
-function createTempArray(stringArray) {
-  let tempArray = [];
+function createTemplateArray(addressArray) {
+  let templateArray = [];
 
-  for (let i = 0; i < stringArray.length; i++) {
+  for (let i = 0; i < addressArray.length; i++) {
     //Considera qualquer string que não tenha números
-    if (stringArray[i].match(/^([A-z]+)([^\d])/)) {
-      tempArray.push("Word");
+    if (addressArray[i].match(/^([A-z]+)([^\d])/)) {
+      templateArray.push("Word");
     }
     //Considera qualquer string que tenha um dígito
-    if (stringArray[i].match(/\d/)) {
-      tempArray.push(1);
+    if (addressArray[i].match(/\d/)) {
+      templateArray.push(1);
     }
   }
   //Criação do array temporário para checagem/indexagem
-  return tempArray;
+  return templateArray;
 }
 
 //Checa se o array tem 2 números, retorna um objeto com Boolean, e os indexes dos 2 números caso existam
-function hasTwoNumber(tempArray) {
-  let arrayAnalysed = {
+function hasTwoNumber(templateArray) {
+  let analysedArray = {
     hasTwoNumber: false,
     number1Index: null,
     number2Index: null,
   };
-  for (let i = 0; i < tempArray.length; i++) {
-    if (tempArray[i] === 1 && arrayAnalysed.number1Index === null) {
-      let index1 = tempArray.indexOf(tempArray[i]);
-      arrayAnalysed.number1Index = index1;
+  for (let i = 0; i < templateArray.length; i++) {
+    if (templateArray[i] === 1 && analysedArray.number1Index === null) {
+      let index1 = templateArray.indexOf(templateArray[i]);
+      analysedArray.number1Index = index1;
       i++;
     }
-    if (tempArray[i] === 1 && arrayAnalysed.number1Index !== null) {
+    if (templateArray[i] === 1 && analysedArray.number1Index !== null) {
       let index2 = i;
-      arrayAnalysed.number2Index = index2;
-      arrayAnalysed.hasTwoNumber = true;
+      analysedArray.number2Index = index2;
+      analysedArray.hasTwoNumber = true;
     }
   }
-  return arrayAnalysed;
+  return analysedArray;
 }
 
 //Utiliza o conhecimento do index para cortar o array de forma correta e atualizar os valores de address
@@ -65,51 +65,47 @@ function twoNumberAddress(addressArray, analysedArray) {
 }
 
 //Utiliza o conhecimento do index para cortar o array no número da casa e o que vier após
-function nationalAddress(stringArray, analysedArray) {
-  address.street = stringArray.slice(0, analysedArray.number1Index).join(" ");
-  address.number = stringArray.slice(analysedArray.number1Index).join(" ");
+function nationalAddress(addressArray, analysedArray) {
+  address.street = addressArray.slice(0, analysedArray.number1Index).join(" ");
+  address.number = addressArray.slice(analysedArray.number1Index).join(" ");
 }
 
 //Caso o index de dígito seja 0 adiciona array[0] como número, corta o array a partir dele e atribui à rua
-function internationalAddress(stringArray) {
-  address.number = stringArray[0];
-  address.street = stringArray.slice(1).join(" ");
+function internationalAddress(addressArray) {
+  address.number = addressArray[0];
+  address.street = addressArray.slice(1).join(" ");
 }
 
 for (let i = 0; i < addresses.length; i++) {
-  var splitAddress = addresses[i].split(/\s/);
+  let splitAddress = addresses[i].split(/\s/);
 
   //Retirada de vírgulas
   for (let j = 0; j < splitAddress.length; j++) {
     splitAddress[j] = splitAddress[j].replace(/\,/, "");
   }
 
-  let mockArray = createTempArray(splitAddress);
+  let mockArray = createTemplateArray(splitAddress);
 
-  let analyseMockArray = hasTwoNumber(mockArray);
+  let mockAnalysedArray = hasTwoNumber(mockArray);
 
-  if (analyseMockArray.hasTwoNumber === true) {
-    twoNumberAddress(splitAddress, analyseMockArray);
+  if (mockAnalysedArray.hasTwoNumber) {
+    twoNumberAddress(splitAddress, mockAnalysedArray);
   }
-  if (
-    analyseMockArray.hasTwoNumber !== true &&
-    analyseMockArray.number1Index === 0
-  ) {
+  if (!mockAnalysedArray.hasTwoNumber && mockAnalysedArray.number1Index === 0) {
     internationalAddress(splitAddress);
   }
-  if (
-    analyseMockArray.hasTwoNumber !== true &&
-    analyseMockArray.number1Index !== 0
-  ) {
-    nationalAddress(splitAddress, analyseMockArray);
+  if (!mockAnalysedArray.hasTwoNumber && mockAnalysedArray.number1Index !== 0) {
+    nationalAddress(splitAddress, mockAnalysedArray);
   }
 
   if (address.street.match(/\s$/)) {
+    //Retirada de espaços vazios ao fim das strings
     let tempStr = address.street.replace(/\s$/, "");
-    address.street = tempStr; //Retirada de espaços vazios
+    address.street = tempStr;
   }
 
   console.log(address);
+  //Limpando address para o próximo input
   address.street = "";
   address.number = "";
 }
